e565fa58d52662c588f480af078300864f8cf152
prajwal <prajwalkale71234@gmail.com>
2022-06-21 18:25:19 +0530

    adding filter asssets

--- lib/Class_package/Enum.dart
+++ lib/Class_package/Enum.dart
@@ -28,3 +28,13 @@ List<String> SKBanner = [
   'assets/SKBanner/banner4.jpg',
   'assets/SKBanner/banner5.jpg'
 ];
+
+List<String> SpecIndex = [
+  'assets/Glasses/Aviator.png',
+  'assets/Glasses/Cat eye.png',
+  'assets/Glasses/Rectangle.png',
+  'assets/Glasses/Round.png',
+  'assets/Glasses/Wayfarer.png'
+];
+
+List<String> SpecName = ['Aviator', 'Cat', 'Rectangle', 'Round', 'Wayfarer'];
--- lib/SpecsKart/SpecsKart.dart
+++ lib/SpecsKart/SpecsKart.dart
@@ -12,55 +12,76 @@ class SpescKart extends StatefulWidget {
 
 class _SpescKartState extends State<SpescKart> {
   int activeindex = 0;
+
+  Widget BuildCard({required int index}) {
+    return Container(
+      color: Colors.white,
+      child: GridTile(
+        child: Image.asset(SpecIndex[index]),
+        footer: Text(SpecName[index]),
+      ),
+    );
+  }
+
+  Widget buildGridView() => GridView.builder(
+        shrinkWrap: true,
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemCount: 5,
+        itemBuilder: (context, int indx) => BuildCard(index: indx),
+      );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xff232D2A),
+        backgroundColor: const Color(0xff232D2A),
         appBar: AppBar(
           centerTitle: true,
-          backgroundColor: Color(0xff203243),
+          backgroundColor: const Color(0xff203243),
           title: Text(
             'Spec\'s Kart',
-            style: GoogleFonts.roboto(fontSize: 20),
+            style:
+                GoogleFonts.roboto(fontSize: 20, fontWeight: FontWeight.bold),
           ),
         ),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            CarouselSlider.builder(
-              options: CarouselOptions(
-                  height: 200,
-                  autoPlay: true,
-                  //enableInfiniteScroll: false,
-                  onPageChanged: (index, reason) {
-                    setState(() => activeindex = index);
-                  },
-                  pageSnapping: true,
-                  autoPlayInterval: const Duration(seconds: 6),
-                  //enlargeCenterPage: true,
-                  enlargeStrategy: CenterPageEnlargeStrategy.scale),
-              itemCount: SKBanner.length,
-              itemBuilder: (context, index, rindex) => Card(
-                color: Colors.greenAccent.shade100,
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10))),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.redAccent,
-                        borderRadius: BorderRadius.circular(30)),
-                    //height: 10,
-                    width: 800,
-                    child: Image.asset(
-                      SKBanner[index],
-                      //height: 10,
-                      width: 800,
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                ),
-              ),
-            ),
+            // CarouselSlider.builder(
+            //   options: CarouselOptions(
+            //       height: 200,
+            //       autoPlay: true,
+            //       //enableInfiniteScroll: false,
+            //       onPageChanged: (index, reason) {
+            //         setState(() => activeindex = index);
+            //       },
+            //       pageSnapping: true,
+            //       autoPlayInterval: const Duration(seconds: 6),
+            //       //enlargeCenterPage: true,
+            //       enlargeStrategy: CenterPageEnlargeStrategy.scale),
+            //   itemCount: SKBanner.length,
+            //   itemBuilder: (context, index, rindex) => Card(
+            //     color: Colors.greenAccent.shade100,
+            //     shape: const RoundedRectangleBorder(
+            //         borderRadius: BorderRadius.all(Radius.circular(10))),
+            //     child: Padding(
+            //       padding: const EdgeInsets.all(8.0),
+            //       child: Container(
+            //         decoration: BoxDecoration(
+            //             color: Colors.redAccent,
+            //             borderRadius: BorderRadius.circular(30)),
+            //         //height: 10,
+            //         width: 800,
+            //         child: Image.asset(
+            //           SKBanner[index],
+            //           //height: 10,
+            //           width: 800,
+            //           fit: BoxFit.fill,
+            //         ),
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            buildGridView(),
           ],
         ));
   }
